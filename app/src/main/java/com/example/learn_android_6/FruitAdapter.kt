16995c88698ec0419c5a086fd201f2a04da91ea4
zzmeow0516 <zzmeow0516@163.com>
2024-04-27 23:38:59 +0800

    recyclerview's setOnClickListener

--- app/src/main/java/com/example/learn_android_6/FruitAdapter.kt
+++ app/src/main/java/com/example/learn_android_6/FruitAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 //参数fruitlist表示数据源
@@ -22,8 +23,33 @@ class FruitAdapter(val fruitList: List<Fruits>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         //加载fruit_item布局
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+
+        //准备注册点击事件
+        val viewHolder = ViewHolder(view)
+        //可以单独为  子项布局  或者是  子项布局中的每一个控件  注册监听
+        //itemView是最外层的布局
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.absoluteAdapterPosition
+            val fruitItem = fruitList[position]
+            Toast.makeText(parent.context, "we clicked view: ${fruitItem.name}", Toast.LENGTH_SHORT).show()
+        }
+        //点击图片注册一个listener
+        viewHolder.fruitImage.setOnClickListener {
+            val position = viewHolder.absoluteAdapterPosition
+            val fruitItem = fruitList[position]
+            Toast.makeText(parent.context, "we clicked image: ${fruitItem.name}", Toast.LENGTH_SHORT).show()
+        }
+        //点击文字也注册一个listener
+        viewHolder.fruitName.setOnClickListener {
+            val position = viewHolder.absoluteAdapterPosition
+            val fruitItem = fruitList[position]
+            Toast.makeText(parent.context, "we clicked name: ${fruitItem.name}", Toast.LENGTH_SHORT).show()
+        }
+
         //将加载的fruit_item布局传入到viewholder构造函数，从而返回一个viewholder实例
-        return ViewHolder(view)
+        //return ViewHolder(view)
+
+        return viewHolder
     }
 
     //给recyclerview的子项赋值
